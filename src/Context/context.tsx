73ffb198c8866cb4153e55dee1157cf977cbaffd
navin@1503 <79439418+khievnavin@users.noreply.github.com>
2024-03-02 17:36:40 +0700

    Storage

--- src/Context/context.tsx
+++ src/Context/context.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import React, { ReactNode } from "react";
-import { useContext, useState, createContext } from "react";
+import React, { ReactNode, useEffect } from "react";
+import { useContext, useState, createContext } from "react"; 
+import { getLocalStorage, setLocalStorage } from "@/utils/LocalStorage";
+
 
 // Define the type for user information
 interface UserInfo {
@@ -41,47 +43,68 @@ interface MyContextProps {
 // create a provider component
 export const MyContextProvider: React.FC<MyContextProps> = ({ children }) => {
   // store state
-  const [userInfo, setuserInfo] = useState([
-   
-  ]);
+  const [userInfo, setuserInfo] = useState([]);
 
   const [selectUser, setselectUser] = useState("");
 
   // handle Delete Card
   const handleDelete = (id: string) => {
     const newUser = userInfo.filter((item) => item.id !== id);
+    setLocalStorage("userInfo" , newUser)
     setuserInfo(newUser);
+
     // If the deleted card was selected, clear the selection
-    if (selectUser === id) {
-      setselectUser("");
-    }
+    // if (selectUser === id) {
+    //   setselectUser("");
+    // }
+
   };
   const updateUser = (user, selectCard: string) => {
-    setuserInfo((prevUsers) => {
-      return prevUsers.map((prevUser) => {
-        if (prevUser.id === selectCard) {
-          return {
-            ...prevUser,
-            ...user,
-          };
-        }
-        return prevUser;
-      });
+    const newUser = userInfo.map((userInfo)=> {
+      if (userInfo.id === selectCard){
+        return {
+           ...userInfo,
+           ...user,
+        };
+      }
+      return userInfo;
     });
+    setLocalStorage("userInfo", newUser);
+    setuserInfo(newUser);
+
+    // setuserInfo((prevUsers) => {
+    //   return prevUsers.map((prevUser) => {
+    //     if (prevUser.id === selectCard) {
+    //       return {
+    //         ...prevUsers,
+    //         ...user
+    //       };
+    //     }
+    //     return prevUser;
+    //   });
+    
+    // });
   };
-    const handleFormAdd=(userInfo)=>{
+    const handleFormAdd=(userInfo : any)=>{
     const newId = Math.random().toString(36).substring(2, 8); // return 1f74e
         const newUser = { ...userInfo, id: newId };
         setuserInfo((prevUsers) => {
-          return [...prevUsers, newUser];
+          const newAllUsers = [...prevUsers,newUser]
+          setLocalStorage("userInfor", newAllUsers)
+          return newAllUsers;
         });
-  }
+        
+  };
 
   // handle Update
 
   // find selectuser
-  const findUser = userInfo.find((item) => item.id === selectUser);
+  const findUser = userInfo.find((item) => item.id === selectUser); 
 
+  useEffect(() => {
+    const userStorage = getLocalStorage("userInfor") ? getLocalStorage("userInfor") : [];
+    setuserInfo(userStorage);
+  }, []);
   // Provide the shared state and update function to the context
   const contextValue = {
     userInfo,
